02fb5eaf187ce8eb33a7bd6d0c45669a9e52eaa8
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-04-29 10:33:28 +0100

    Testing key bindings for undo/redo.

--- python/nammu/view/AtfEditArea.py
+++ python/nammu/view/AtfEditArea.py
@@ -1,6 +1,6 @@
 from java.awt import Font
-from javax.swing import JTextPane, BorderFactory
-from java.awt.event import MouseAdapter
+from javax.swing import JTextPane, BorderFactory, AbstractAction, KeyStroke
+from java.awt.event import MouseAdapter, KeyEvent
 
 
 class AtfEditArea(JTextPane):
@@ -13,6 +13,15 @@ class AtfEditArea(JTextPane):
         # Consume mouse events when over this JTextPane
         listener = CustomMouseListener(self)
         self.addMouseListener(listener)
+        # Assign key bindings for undo/redo
+        undo_action = UndoAction()
+        redo_action = RedoAction()
+        self.getInputMap().put(KeyStroke.getKeyStroke("F2"),
+                            "doSomething")
+        self.getActionMap().put("doSomething", undo_action)
+        # self.getActionMap().put(KeyEvent.VK_Z, undo_action)
+        # self.getActionMap().put(KeyEvent.VK_Y, redo_action)
+
 
 
     def getToolTipText(self, event=None):
@@ -47,3 +56,27 @@ class CustomMouseListener(MouseAdapter):
         self.panel = panel
     def mousePressed(self, event):
         offset = self.panel.viewToModel(event.getPoint())
+
+
+class UndoAction(AbstractAction):
+    """
+    Needed by JTextPane's actionMap to assing the undo action to CTRL+Z key
+    bindings.
+    """
+    def __init__(self):
+        print "Creating undo action"
+
+    def actionPerformed(self, event=None):
+        print "Executing undo action"
+
+
+class RedoAction(AbstractAction):
+    """
+    Needed by JTextPane's actionMap to assing the redo action to CTRL+Z key
+    bindings.
+    """
+    def __init__(self):
+        print "Creating redo action"
+
+    def actionPerformed(self, event=None):
+        print "Executing redo action"
